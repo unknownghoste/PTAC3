8ce8ac290e38fe26ca065b0fdfd3c8b933203467
AsGhrK <anadantas1994@gmail.com>
2023-06-05 17:17:07 -0400

    1

---
+++ db.js
@@ -0,0 +1,21 @@
+const { Pool } = require('pg');
+
+let connect = async function () {
+  try {
+    if (global.connection) {
+      return Promise.resolve(global.connection);
+    }
+
+    const pool = new Pool({
+      connectionString: 'postgres://postgres:postgres@localhost/ptas2' /* nome do banco de dados. Conecta com o banco. o primeiro é o banco, o segundo é a senha o terceiro é onde está localmente e o último é a pasta*/
+    });
+
+    global.connection = pool;
+    return Promise.resolve(pool);
+  } catch (error) {
+    console.error('Erro ao estabelecer a conexão:', error);
+    throw error;
+  }
+};
+
+module.exports = { connect };
